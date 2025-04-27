08bd00a0f8e41265ee441eb00cb9bd4787ee0121
Yasin Ehsani <yasin.ehsani@volumental.com>
2025-04-27 19:15:05 +0200

    Initial Commit

---
+++ AppostropheTest/AppostropheTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppostropheTestApp.swift
+//  AppostropheTest
+//
+//  Created by Yasin Ehsani on 2025-04-27.
+//
+
+import SwiftUI
+
+@main
+struct AppostropheTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AppostropheTest/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AppostropheTest
+//
+//  Created by Yasin Ehsani on 2025-04-27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
